4eeabbe73d02927aeba335c047d2df8f0d7e76a7
rofl0r <retnyg@gmx.net>
2012-11-14 23:28:13 +0100

    start cleanup string funcs

--- Engine/ac.cpp
+++ Engine/ac.cpp
@@ -11,9 +11,8 @@
   CLEAR that the code has been altered from the Standard Version.
 
 */
-extern "C" {
-	#include "Clib32.h"
-}
+#include "Clib32.h"
+#include <stdlib.h>
 
 #if !defined(IOS_VERSION) && !defined(PSP_VERSION) && !defined(ANDROID_VERSION)
 int psp_video_framedrop = 1;
@@ -1855,6 +1854,9 @@ void PlayAmbientSound (int channel, int sndnum, int vol, int x, int y) {
 
 /*
 #include "almp3_old.h"
+#include <string.h>
+#include <string.h>
+#include <string.h>
 ALLEGRO_MP3 *mp3ptr;
 int mp3vol=128;
 
@@ -10894,6 +10896,7 @@ void* ScriptString::CreateString(const char *fromText) {
 }
 
 int ScriptString::Dispose(const char *address, bool force) {
+	return 1;
   // always dispose
   if (text) {
 /*    char buffer[1000];
@@ -10911,7 +10914,7 @@ const char *ScriptString::GetType() {
 
 int ScriptString::Serialize(const char *address, char *buffer, int bufsize) {
   if (text == NULL)
-    text = "";
+    text = strdup("");
   StartSerialize(buffer);
   SerializeInt(strlen(text));
   strcpy(&serbuffer[bytesSoFar], text);
@@ -10949,156 +10952,159 @@ const char* String_Copy(const char *srcString) {
 }
 
 const char* String_Append(const char *thisString, const char *extrabit) {
-  char *buffer = (char*)malloc(strlen(thisString) + strlen(extrabit) + 1);
-  strcpy(buffer, thisString);
-  strcat(buffer, extrabit);
-  return CreateNewScriptString(buffer, false);
+	size_t l1= strlen(thisString), l2 = strlen(extrabit);
+	char *buffer = (char*)malloc(l1 + l2 + 1);
+	memcpy(buffer, thisString, l1);
+	memcpy(buffer + l1, extrabit, l2);
+	buffer[l1+l2] = 0;
+	return CreateNewScriptString(buffer, false);
 }
 
 const char* String_AppendChar(const char *thisString, char extraOne) {
-  char *buffer = (char*)malloc(strlen(thisString) + 2);
-  sprintf(buffer, "%s%c", thisString, extraOne);
-  return CreateNewScriptString(buffer, false);
+	size_t l = thisString? strlen(thisString) : 0;
+	char *buffer = (char*)malloc(l + 2);
+	if(buffer) {
+		if(l) memcpy(buffer, thisString, l);
+		sprintf(buffer + l, "%c", extraOne);
+	}
+	return CreateNewScriptString(buffer, false);
 }
 
 const char* String_ReplaceCharAt(const char *thisString, int index, char newChar) {
-  if ((index < 0) || (index >= (int)strlen(thisString)))
-    quit("!String.ReplaceCharAt: index outside range of string");
+	if(!thisString) return 0;
+	size_t l = strlen(thisString);
+	if ((index < 0) || ((unsigned) index >= l))
+		quit("!String.ReplaceCharAt: index outside range of string");
 
-  char *buffer = (char*)malloc(strlen(thisString) + 1);
-  strcpy(buffer, thisString);
-  buffer[index] = newChar;
-  return CreateNewScriptString(buffer, false);
+	char *buffer = (char*)malloc(l + 1);
+	memcpy(buffer, thisString, l+1);
+	strcpy(buffer, thisString);
+	buffer[index] = newChar;
+	return CreateNewScriptString(buffer, false);
 }
 
 const char* String_Truncate(const char *thisString, int length) {
-  if (length < 0)
-    quit("!String.Truncate: invalid length");
+	if (length < 0)
+		quit("!String.Truncate: invalid length");
+	if(!thisString) return 0;
+	size_t l = strlen(thisString);
 
-  if (length >= (int)strlen(thisString))
-    return thisString;
+	if ((unsigned) length >= l)
+		return thisString;//CreateNewScriptString(strdup(thisString), false);
 
-  char *buffer = (char*)malloc(length + 1);
-  strncpy(buffer, thisString, length);
-  buffer[length] = 0;
-  return CreateNewScriptString(buffer, false);
+	char *buffer = (char*)malloc(length + 1);
+	strncpy(buffer, thisString, length);
+	buffer[length] = 0;
+	return CreateNewScriptString(buffer, false);
 }
 
 const char* String_Substring(const char *thisString, int index, int length) {
-  if (length < 0)
-    quit("!String.Substring: invalid length");
-  if ((index < 0) || (index > (int)strlen(thisString)))
-    quit("!String.Substring: invalid index");
+	if (length < 0)
+		quit("!String.Substring: invalid length");
+	if(!thisString) return 0;
+	size_t l = strlen(thisString);
+	if ((index < 0) || ((unsigned) index > l))
+	quit("!String.Substring: invalid index");
+	if(index + length > l)
+		length = l - index;
 
-  char *buffer = (char*)malloc(length + 1);
-  strncpy(buffer, &thisString[index], length);
-  buffer[length] = 0;
-  return CreateNewScriptString(buffer, false);
+	char *buffer = (char*)malloc(length + 1);
+	if(buffer) {
+		memcpy(buffer, thisString + index, length);
+		buffer[length] = 0;
+	}
+	return CreateNewScriptString(buffer, false);
+}
+
+static int mystreq(const char* cmp1, const char *cmp2, bool caseSensitive) {
+	if (caseSensitive) 
+		return strcmp (cmp1, cmp2) == 0 ? 1 : 0;
+	else
+		return stricmp(cmp1, cmp2) == 0 ? 1 : 0;
 }
 
 int String_CompareTo(const char *thisString, const char *otherString, bool caseSensitive) {
-
-  if (caseSensitive) {
-    return strcmp(thisString, otherString);
-  }
-  else {
-    return stricmp(thisString, otherString);
-  }
+	const char *cmp1 = thisString ? thisString : "";
+	const char *cmp2 = otherString ? otherString : "";
+	if (caseSensitive) 
+		return strcmp (cmp1, cmp2);
+	else
+		return stricmp(cmp1, cmp2);
 }
 
 int String_StartsWith(const char *thisString, const char *checkForString, bool caseSensitive) {
-
-  if (caseSensitive) {
-    return (strncmp(thisString, checkForString, strlen(checkForString)) == 0) ? 1 : 0;
-  }
-  else {
-    return (strnicmp(thisString, checkForString, strlen(checkForString)) == 0) ? 1 : 0;
-  }
+	const char *cmp1 = thisString ? thisString : "";
+	const char *cmp2 = checkForString ? checkForString : "";
+	size_t l = strlen(cmp2);
+	return mystreq(cmp1, cmp2, l);
 }
 
 int String_EndsWith(const char *thisString, const char *checkForString, bool caseSensitive) {
-
-  int checkAtOffset = strlen(thisString) - strlen(checkForString);
-
-  if (checkAtOffset < 0)
-  {
-    return 0;
-  }
-
-  if (caseSensitive) 
-  {
-    return (strcmp(&thisString[checkAtOffset], checkForString) == 0) ? 1 : 0;
-  }
-  else 
-  {
-    return (stricmp(&thisString[checkAtOffset], checkForString) == 0) ? 1 : 0;
-  }
+	if (!thisString || !checkForString) return 0;
+	
+	ptrdiff_t checkAtOffset = strlen(thisString) - strlen(checkForString);
+	if (checkAtOffset < 0) return 0;
+	return mystreq(thisString + checkAtOffset, checkForString, caseSensitive);
 }
 
-const char* String_Replace(const char *thisString, const char *lookForText, const char *replaceWithText, bool caseSensitive)
-{
-  char resultBuffer[STD_BUFFER_SIZE] = "";
-  int thisStringLen = (int)strlen(thisString);
-  int outputSize = 0;
-  for (int i = 0; i < thisStringLen; i++)
-  {
-    bool matchHere = false;
-    if (caseSensitive)
-    {
-      matchHere = (strncmp(&thisString[i], lookForText, strlen(lookForText)) == 0);
-    }
-    else
-    {
-      matchHere = (strnicmp(&thisString[i], lookForText, strlen(lookForText)) == 0);
-    }
+const char* String_Replace(const char *thisString, const char *lookForText, 
+			   const char *replaceWithText, bool caseSensitive) {
+	char resultBuffer[STD_BUFFER_SIZE] = "";
+	size_t i, thisStringLen = strlen(thisString), 
+	       l = strlen(lookForText), r = strlen(replaceWithText),
+	       outputSize = 0;
+	       
+	for (i = 0; i < thisStringLen; i++) {
+		bool matchHere = false;
+		if (caseSensitive)
+			matchHere = (strncmp(thisString + i, lookForText, l) == 0);
+		else
+			matchHere = (strnicmp(thisString + i, lookForText, l) == 0);
 
-    if (matchHere)
-    {
-      strcpy(&resultBuffer[outputSize], replaceWithText);
-      outputSize += strlen(replaceWithText);
-      i += strlen(lookForText) - 1;
-    }
-    else
-    {
-      resultBuffer[outputSize] = thisString[i];
-      outputSize++;
-    }
-  }
+		if (matchHere) {
+			snprintf(resultBuffer + outputSize, sizeof(resultBuffer) - outputSize, "%s", replaceWithText);
+			outputSize += r;
+			i += l - 1;
+		} else {
+			resultBuffer[outputSize] = thisString[i];
+			outputSize++;
+		}
+	}
 
-  resultBuffer[outputSize] = 0;
+	resultBuffer[outputSize] = 0;
 
-  return CreateNewScriptString(resultBuffer, true);
+	return CreateNewScriptString(resultBuffer, true);
 }
 
 const char* String_LowerCase(const char *thisString) {
-  char *buffer = (char*)malloc(strlen(thisString) + 1);
-  strcpy(buffer, thisString);
-  strlwr(buffer);
-  return CreateNewScriptString(buffer, false);
+	if(!thisString) return 0;
+	char *buffer = strdup(thisString);
+	strlwr(buffer);
+	return CreateNewScriptString(buffer, false);
 }
 
 const char* String_UpperCase(const char *thisString) {
-  char *buffer = (char*)malloc(strlen(thisString) + 1);
-  strcpy(buffer, thisString);
-  strupr(buffer);
-  return CreateNewScriptString(buffer, false);
+	if(!thisString) return 0;
+	char *buffer = strdup(thisString);
+	strupr(buffer);
+	return CreateNewScriptString(buffer, false);
 }
 
 const char* String_Format(const char *texx, ...) {
-  char displbuf[STD_BUFFER_SIZE];
+	char displbuf[STD_BUFFER_SIZE];
 
-  va_list ap;
-  va_start(ap,texx);
-  my_sprintf(displbuf, get_translation(texx), ap);
-  va_end(ap);
+	va_list ap;
+	va_start(ap,texx);
+	my_sprintf(displbuf, get_translation(texx), ap);
+	va_end(ap);
 
-  return CreateNewScriptString(displbuf);
+	return CreateNewScriptString(displbuf);
 }
 
 int String_GetChars(const char *texx, int index) {
-  if ((index < 0) || (index >= (int)strlen(texx)))
-    return 0;
-  return texx[index];
+	if ((index < 0) || ((unsigned) index >= strlen(texx)))
+		return 0;
+	return texx[index];
 }
 
 
@@ -21966,99 +21972,104 @@ int _sc_stricmp (char *s1, char *s2) {
 
 // 64 bit: Not sure if this function is 64 bit ready
 // Custom printf, needed because floats are pushed as 8 bytes
+// WTF
+// WTF
+// WTF
+// WTF
+// WTF
+// WTF
+// this is the biggest crap i've ever seen
 void my_sprintf(char *buffer, const char *fmt, va_list ap) {
-  int bufidx = 0;
-  const char *curptr = fmt;
-  const char *endptr;
-  char spfbuffer[STD_BUFFER_SIZE];
-  char fmtstring[100];
-  int numargs = -1;
+	int bufidx = 0;
+	const char *curptr = fmt;
+	const char *endptr;
+	char spfbuffer[STD_BUFFER_SIZE];
+	char fmtstring[100];
+	int numargs = -1;
 
-  while (1) {
-    // copy across everything until the next % (or end of string)
-    endptr = strchr(curptr, '%');
-    if (endptr == NULL)
-      endptr = &curptr[strlen(curptr)];
-    while (curptr < endptr) {
-      buffer[bufidx] = *curptr;
-      curptr++;
-      bufidx++;
-    }
-    // at this point, curptr and endptr should be equal and pointing
-    // to the % or \0
-    if (*curptr == 0)
-      break;
-    if (curptr[1] == '%') {
-      // "%%", so just write a % to the output
-      buffer[bufidx] = '%';
-      bufidx++;
-      curptr += 2;
-      continue;
-    }
-    // find the end of the % clause
-    while ((*endptr != 'd') && (*endptr != 'f') && (*endptr != 'c') &&
-           (*endptr != 0) && (*endptr != 's') && (*endptr != 'x') &&
-           (*endptr != 'X'))
-      endptr++;
+	while (1) {
+		// copy across everything until the next % (or end of string)
+		endptr = strchr(curptr, '%');
+		if (endptr == NULL)
+		endptr = &curptr[strlen(curptr)];
+		while (curptr < endptr) {
+			buffer[bufidx] = *curptr;
+			curptr++;
+			bufidx++;
+		}
+		// at this point, curptr and endptr should be equal and pointing
+		// to the % or \0
+		if (*curptr == 0)
+			break;
+		if (curptr[1] == '%') {
+			// "%%", so just write a % to the output
+			buffer[bufidx] = '%';
+			bufidx++;
+			curptr += 2;
+			continue;
+		}
+		// find the end of the % clause
+		while ((*endptr != 'd') && (*endptr != 'f') && (*endptr != 'c') &&
+			(*endptr != 0) && (*endptr != 's') && (*endptr != 'x') &&
+			(*endptr != 'X'))
+		endptr++;
 
-    if (numargs >= 0) {
-      numargs--;
-      // if there are not enough arguments, just copy the %d
-      // to the output string rather than trying to format it
-      if (numargs < 0)
-        endptr = &curptr[strlen(curptr)];
-    }
+		if (numargs >= 0) {
+		numargs--;
+		// if there are not enough arguments, just copy the %d
+		// to the output string rather than trying to format it
+		if (numargs < 0)
+			endptr = &curptr[strlen(curptr)];
+		}
 
-    if (*endptr == 0) {
-      // something like %p which we don't support, so just write
-      // the % to the output
-      buffer[bufidx] = '%';
-      bufidx++;
-      curptr++;
-      continue;
-    }
-    // move endptr to 1 after the end character
-    endptr++;
+		if (*endptr == 0) {
+			// something like %p which we don't support, so just write
+			// the % to the output
+			buffer[bufidx] = '%';
+			bufidx++;
+			curptr++;
+			continue;
+		}
+		// move endptr to 1 after the end character
+		endptr++;
 
-    // copy the %d or whatever
-    strncpy(fmtstring, curptr, (endptr - curptr));
-    fmtstring[endptr - curptr] = 0;
+		// copy the %d or whatever
+		strncpy(fmtstring, curptr, (endptr - curptr));
+		fmtstring[endptr - curptr] = 0;
+		
+		union FOOLZ {
+			unsigned long l;
+			float f;
+		} theArg = va_arg(ap, union FOOLZ);
 
-    unsigned int theArg = va_arg(ap, unsigned int);
+		// use sprintf to parse the actual %02d type thing
+		if (endptr[-1] == 'f') {
+			// floats are pushed as 8-bytes, so ensure that it knows this is a float
+			float floatArg = theArg.f;
+			sprintf(spfbuffer, fmtstring, floatArg);
+		}
+		else if ((theArg.l == (size_t) buffer) && (endptr[-1] == 's'))
+			quit("Cannot use destination as argument to StrFormat");
+		else if ((theArg.l < 0x10000) && (endptr[-1] == 's'))
+			quit("!One of the string arguments supplied was not a string");
+		else if (endptr[-1] == 's') {
+			strncpy(spfbuffer, (const char*)theArg.l, STD_BUFFER_SIZE);
+			spfbuffer[STD_BUFFER_SIZE - 1] = 0;
+		}
+		else 
+			sprintf(spfbuffer, fmtstring, theArg.l);
 
-    // use sprintf to parse the actual %02d type thing
-    if (endptr[-1] == 'f') {
-      // floats are pushed as 8-bytes, so ensure that it knows this is a float
-      float floatArg;
-      memcpy(&floatArg, &theArg, sizeof(float));
-      sprintf(spfbuffer, fmtstring, floatArg);
-    }
-/*  64 bit: Not compatible
-    else if ((theArg == (int)buffer) && (endptr[-1] == 's'))
-      quit("Cannot use destination as argument to StrFormat");
-*/
-    else if ((theArg < 0x10000) && (endptr[-1] == 's'))
-      quit("!One of the string arguments supplied was not a string");
-    else if (endptr[-1] == 's')
-    {
-      strncpy(spfbuffer, (const char*)theArg, STD_BUFFER_SIZE);
-      spfbuffer[STD_BUFFER_SIZE - 1] = 0;
-    }
-    else 
-      sprintf(spfbuffer, fmtstring, theArg);
+		// use the formatted text
+		buffer[bufidx] = 0;
 
-    // use the formatted text
-    buffer[bufidx] = 0;
+		if (bufidx + strlen(spfbuffer) >= STD_BUFFER_SIZE)
+			quitprintf("!String.Format: buffer overrun: maximum formatted string length %d chars, this string: %d chars", STD_BUFFER_SIZE, bufidx + strlen(spfbuffer));
 
-    if (bufidx + strlen(spfbuffer) >= STD_BUFFER_SIZE)
-      quitprintf("!String.Format: buffer overrun: maximum formatted string length %d chars, this string: %d chars", STD_BUFFER_SIZE, bufidx + strlen(spfbuffer));
-
-    strcat(buffer, spfbuffer);
-    bufidx += strlen(spfbuffer);
-    curptr = endptr;
-  }
-  buffer[bufidx] = 0;
-
+		strcat(buffer, spfbuffer);
+		bufidx += strlen(spfbuffer);
+		curptr = endptr;
+	}
+	buffer[bufidx] = 0;
 }
 
 void _sc_AbortGame(char*texx, ...) {
